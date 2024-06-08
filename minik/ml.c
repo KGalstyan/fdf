805e3d2aba43519cb9e576b600afee7dedd37704
Karen Galstyan <kgalstya@c2r8s5.42yerevan.am>
2024-06-08 22:42:25 +0400

    loading...

--- minik/ml.c
+++ minik/ml.c
@@ -9,9 +9,9 @@ void    mlx_pixel_put_img(t_img *img, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-// int	is_steep()
+// int	is_steep(int dy, int dx);
 // {
-//      return (dy > dx);
+//     return (dy > dx);
 // }
 
 // void  isometric(t_point cord)
@@ -34,7 +34,6 @@ void draw_line(t_img *img, t_data cord)
 	int i = 0;
 
     // t_point point;
-
 	if(dx > dy)
     {
         i = -1;
@@ -63,14 +62,20 @@ void draw_line(t_img *img, t_data cord)
         p = 2 * tx - ty;
         i = 0;
         mlx_pixel_put_img(img, x, y, img->color);
-        while(i < ty)
+        while(i < dy)
         {
-            y++;
-            if(p < 0)
+            if(ty > 0)
+                y++;
+            else
+                y--;
+            if(p < 0 && tx > 0)
               p = p + (2 * tx);
             else
             {
-                x++;
+                if(tx > 0)
+                    x++;
+                else
+                    x--;
                 p = p + (2 * tx) - (2 * ty);
             }
             mlx_pixel_put_img(img, x, y, img->color);
@@ -78,41 +83,6 @@ void draw_line(t_img *img, t_data cord)
         }
     }
 }
-    // int sx = cord.x1 < cord.x2 ? 1 : -1;
-    // int sy = cord.y1 < cord.y2 ? 1 : -1;
-
-    // while (x != cord.x2 || y != cord.y2)
-    // {
-    //     if (p >= 0) {
-    //         if (p >= dy) 
-    //         {
-    //             x += sx;
-    //             p -= dy;
-    //         }
-    //         if (p <= dx) 
-    //         {
-    //             y += sy;
-    //             p += dx;
-    //         }
-    //     } 
-    //     else 
-    //     {
-    //         if (p <= -dx) 
-    //         {
-    //             y += sy;
-    //             p += dx;
-    //         }
-    //         if (p >= -dy) 
-    //         {
-    //             x += sx;
-    //             p -= dy;
-    //         }
-    //     }
-    //     mlx_pixel_put_img(img, x, y, img->color);
-    // }
-//}
-
-
 
 int	main(void)
 {
@@ -126,23 +96,16 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 1280, 720);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	img.color = 255;
-	cord.x1 = 500;
-	cord.y1 = 20;
-    cord.x2 = 20;
-    cord.y2 = 500;
-    // while(cord.x1 < 500)
-    // {
-	//     draw_line(&img, cord);
-	//     mlx_put_image_to_window(mlx, mlx_win, img.img, 400, 200);
-    //     cord.x1++;
-    //     cord.y1++;
-    //     cord.x2++;
-    //     cord.y2++;
-    //     printf("%d\n", cord.x1);
-    //     printf("%d\n", cord.x2);
-    //     printf("%d\n", cord.y1);
-    //     printf("%d\n", cord.y2);
-    // }
+	cord.x1 = 200;
+	cord.y1 = 200;
+    cord.x2 = 150;
+    cord.y2 = 300;
+    draw_line(&img, cord);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 400, 200);
+    cord.x1 = 200;
+	cord.y1 = 200;
+    cord.x2 = 100;
+    cord.y2 = 280;
     draw_line(&img, cord);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 400, 200);
 	mlx_loop(mlx);
